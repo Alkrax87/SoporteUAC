121ef37bab68070988b2db3302e44e73081e42c9
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-12 12:16:08 -0500

    Add main color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#00CEFF',
+      }
+    },
   },
   plugins: [],
 }
